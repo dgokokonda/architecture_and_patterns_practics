37b0b18b71f0e29e8e1e911f01ff2a94ff2105c5
Veronika Batrakova <sunny-bvb@mail.ru>
2026-03-10 13:35:03 +0300

    Update 4_singleton.js

--- patterns/1 creational/4_singleton.js
+++ patterns/1 creational/4_singleton.js
@@ -20,3 +20,32 @@ console.log(mongo.getData());
 
 const mysql = new Database("MySQL");
 console.log(mysql.getData()); // вернет также 'MongoDB'
+// ================================
+
+// Более корректный вариант
+class Database {
+  private static instance: Database | null = null;
+  private data: any;
+  
+  // Приватный конструктор - нельзя создать через new снаружи
+  private constructor(data: any) {
+    this.data = data;
+  }
+  
+  // Статический метод для получения экземпляра
+  public static getInstance(data?: any): Database {
+    if (!Database.instance) {
+      Database.instance = new Database(data);
+    }
+    return Database.instance;
+  }
+  
+  public getData() {
+    return this.data;
+  }
+}
+
+// Использование
+const db1 = Database.getInstance({ users: [] });
+const db2 = Database.getInstance();
+console.log(db1 === db2); // true - один экземпляр
